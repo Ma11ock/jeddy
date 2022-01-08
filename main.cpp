f4f34daa946da94ba0556ad4cee2d27d8d368e32
Ryan Jeffrey <ryan@ryanmj.xyz>
2022-01-08 03:06:45 -0800

    Global getters, slow printing of characters.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <cstdint>
 #include <string>
 #include <chrono>
@@ -20,6 +21,22 @@ namespace
     int maxX = 0;
     int maxY = 0;
 
+    inline int getX()
+    {
+        int x = 0;
+        int y = 0;
+        getyx(stdscr, y, x);
+        return x;
+    }
+
+    inline int getY()
+    {
+        int x = 0;
+        int y = 0;
+        getyx(stdscr, y, x);
+        return y;
+    }
+
     struct textEffect
     {
         chron::milliseconds wait;
@@ -31,6 +48,8 @@ namespace
         bool dx = false;
         bool dy = false;
         std::string text;
+        // Wait between printing characters.
+        chron::milliseconds outputWait = 75ms;
         std::function<void()> postRunHook;
 
         void doTextEffect() const
@@ -47,9 +66,22 @@ namespace
             // Print the string if there is one and run the post run
             // hook.
             if(!text.empty())
-                printw("%s", text.c_str());
+            {
+                if(outputWait >= 0ms)
+                {
+                    for(auto c : text)
+                    {
+                        std::this_thread::sleep_for(outputWait);
+                        printw("%c", c);
+                        refresh();
+                    }
+                }
+                else
+                    printw("%s", text.c_str());
+            }
             if(postRunHook)
                 postRunHook();
+            refresh();
         }
     };
 }
@@ -57,20 +89,23 @@ namespace
 int main(int argc, const char * const argv[])
 {
     const static std::array textEffects = {
-        textEffect{ 1000ms, 1, 0, true, true },
-        textEffect{ 1000ms, 1, 0, true, true },
-        textEffect{ 1000ms, 1, 0, true, true },
+        textEffect{ 2000ms, 1, 0, true, true, "Sneed"s, },
+        textEffect{ 2000ms, 1, 0, true, true },
+        textEffect{ 2000ms, 1, 0, true, true },
     };
     // Init ncurses.
     initscr();
     raw();
     noecho();
+    // Init the cursor (set to green and blinking).
+    //std::cout << "\e]PFFFFFFFF " << std::flush;
     // Get the maximum x and y and decrease the maximum y and x values
     // to 0-count them.
     getmaxyx(stdscr, maxY, maxX);
     maxY--;
     maxX--;
 
+    refresh();
     for(const auto &effect : textEffects)
         effect.doTextEffect();
 
